c80f835d6dbb589a464bfc95221cf6aa9bac07f9
programer1983 <programer972@gmail.com>
2022-01-19 19:04:00 +0200

    header layout;

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import Header from "./components/header/Header";
 import TopBar from "./components/topbar/TopBar";
+import Home from "./pages/home/Home";
 
 
 function App() {
   return (
     <>
      <TopBar />
-       
-      
+     <Home />
     </>
   );
 }
