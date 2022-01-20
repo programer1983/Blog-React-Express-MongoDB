4bda10688674f9e1e1f1188b299f29efbef31b61
programer1983 <programer972@gmail.com>
2022-01-20 12:53:33 +0200

    posts  page creation, post page layout;

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,29 @@
+import "./post.css";
+
+export default function Post() {
+  return (
+    <div className="post">
+        <img
+           className="postImage"
+           src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" 
+           alt="" 
+        />
+        <div className="postInfo">
+            <div className="postCats">
+                <span className="postCat">Music</span>
+                <span className="postCat">Life</span>
+            </div>
+            <span className="postTitle">
+                Lorem ipsum dolor sit amet. 
+            </span>
+            <hr />
+            <span className="postDate">1 hour ago </span>
+        </div>
+        <p className="postDesc">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum lobortis odio quis hendrerit. Cras sagittis magna velit, a bibendum augue tincidunt vel. In eu ullamcorper nulla.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum lobortis odio quis hendrerit. Cras sagittis magna velit, a bibendum augue tincidunt vel. In eu ullamcorper nulla.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum lobortis odio quis hendrerit. Cras sagittis magna velit, a bibendum augue tincidunt vel. In eu ullamcorper nulla.
+        </p>
+    </div>
+  ) 
+}
