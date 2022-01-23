9f698443d7188871b46556ccff342839cc71203a
programer1983 <programer972@gmail.com>
2022-01-23 11:31:33 +0200

    creating a post page;

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import Header from "./components/header/Header";
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 
 
 function App() {
   return (
     <>
      <TopBar />
-     <Single />
+     <Write />
     </>
   );
 }
