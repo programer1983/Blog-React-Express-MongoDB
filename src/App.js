dfa7ba099d21c84c17c36507b11ade89eeab0e75
programer1983 <programer972@gmail.com>
2022-01-24 11:10:55 +0200

    creating a settings page;

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/header/Header";
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
+import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
      <TopBar />
-     <Write />
+     <Settings />
     </>
   );
 }
