9ec690f3252e990753d1f97c4fb3511f0c14278b
programer1983 <programer972@gmail.com>
2022-01-24 12:43:01 +0200

    creating a registration page;

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/header/Header";
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
@@ -11,7 +12,7 @@ function App() {
   return (
     <>
      <TopBar />
-     <Login />
+     <Register />
     </>
   );
 }
