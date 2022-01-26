16d1162d44681bc5c72fe21acf0b2a5f93045be8
programer1983 <programer972@gmail.com>
2022-01-26 18:57:30 +0200

    routing;

--- src/App.js
+++ src/App.js
@@ -6,14 +6,23 @@ import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 
 function App() {
+  const user = false
   return (
-    <>
+    <Router>
      <TopBar />
-     <Register />
-    </>
+     <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route path="/register" element={user ? <Home/> : <Register/>} />
+        <Route path="/login" element={user ? <Home/> : <Login/>} />
+        <Route path="/write" element={user ? <Write/> : <Register/>} />
+        <Route path="/settings" element={user ? <Settings/> : <Register/>} />
+        <Route path="/post/:postId" element={<Single/>} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,6 +1,8 @@
 import "./topbar.css"
+import { Link } from "react-router-dom"
 
 export default function TopBar() {
+    const user = false
     return (
         <div className="top">
             <div className="topLeft">
@@ -11,19 +13,41 @@ export default function TopBar() {
             </div>
             <div className="topCenter">
                 <ul className="topList">
-                    <li className="topListItem">HOME</li>
-                    <li className="topListItem">ABOUT</li>
-                    <li className="topListItem">CONTACT</li>
-                    <li className="topListItem">WRITE</li>
-                    <li className="topListItem">LOGOUT</li>
+                    <li className="topListItem">
+                        <Link className="link" to="/">HOME</Link>
+                    </li>
+                    <li className="topListItem">
+                        <Link className="link" to="/">ABOUT</Link>
+                    </li>
+                    <li className="topListItem">
+                        <Link className="link" to="/">CONTACT</Link>
+                    </li>
+                    <li className="topListItem">
+                        <Link className="link" to="/write">WRITE</Link>
+                    </li>
+                    <li className="topListItem">
+                        {user && "LOGOUT"}
+                    </li>
                 </ul>
             </div>
             <div className="topRight">
-                <img
-                   className="topImg"
-                   src="https://funik.ru/wp-content/uploads/2019/07/25a467650a92f2019572-700x525.jpg" 
-                   alt="" 
-                />
+                {user ? (
+                    <img
+                    className="topImg"
+                    src="https://funik.ru/wp-content/uploads/2019/07/25a467650a92f2019572-700x525.jpg" 
+                    alt="" 
+                 />
+                ) : (
+                    <ul className="topList">
+                        <li className="topListItem">
+                            <Link className="link" to="/login">LOGIN</Link>
+                        </li>
+                        <li className="topListItem">
+                            <Link className="link" to="/register">REGISTER</Link>
+                        </li>
+                    </ul>
+                )
+            }
                 <i className="topSearchIcon fas fa-search"></i>
             </div>
         </div>
