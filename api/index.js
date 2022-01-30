68870b21e344585f81bd70ceed5faf7af23368fc
programer1983 <programer972@gmail.com>
2022-01-30 12:51:50 +0200

    update - change user;

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express()
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
 const authRoute = require("./routes/auth")
+const usersRoute = require("./routes/users")
 
 dotenv.config()
 
@@ -16,6 +17,7 @@ mongoose.connect(process.env.MONGO_URL, {
 .catch((err) => console.log(err))
 
 app.use("/api/auth", authRoute)
+app.use("/api/users", usersRoute)
 
 
 app.listen("5000", () => {
