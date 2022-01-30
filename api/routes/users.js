261981486e35ec8a94fcb2c7dd0b65fda923d372
programer1983 <programer972@gmail.com>
2022-01-30 13:17:18 +0200

    deleting a user;

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,6 @@
 const router = require("express").Router()
 const User = require("./../models/User")
+const Post = require("./../models/Post")
 const bcrypt = require("bcrypt")
 
 
@@ -29,6 +30,25 @@ router.put("/:id", async (req, res) => {
 })
 
 //DELETE
+
+router.delete("/:id", async (req, res) => {
+    if(req.body.userId === req.params.id){
+        try {
+            const user = await User.findById(req.params.id)
+            try {
+                await Post.deleteMany({usernmae: user.username})
+                await User.findByIdAndDelete(req.params.id)
+                res.status(200).json("User has been deleted...")
+            }catch(err){
+                res.status(500).json(err)
+            }     
+        }catch(err){
+            res.status(404).json("User not found!")
+        }
+    }else{
+        res.status(401).json("You can delete only your account!")
+    }
+})
 
 
 
