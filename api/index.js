e11f0a36a1c3597852f2fc9bb78b6fac730a5278
programer1983 <programer972@gmail.com>
2022-02-01 12:40:20 +0200

    adding and getting categories;

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose")
 const authRoute = require("./routes/auth")
 const usersRoute = require("./routes/users")
 const postRoute = require("./routes/posts")
+const categoryRoute = require("./routes/categories")
 
 dotenv.config()
 
@@ -20,6 +21,7 @@ mongoose.connect(process.env.MONGO_URL, {
 app.use("/api/auth", authRoute)
 app.use("/api/users", usersRoute)
 app.use("/api/posts", postRoute)
+app.use("/api/categories", categoryRoute)
 
 
 app.listen("5000", () => {
