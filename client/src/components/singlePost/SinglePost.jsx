8bcd86c3048d7e135966488537e31a42ecd8d19f
programer1983 <programer972@gmail.com>
2022-02-02 11:48:41 +0200

    receiving data on a separate post and displaying them;

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,28 +1,44 @@
 import "./singlePost.css"
+import {useLocation} from "react-router-dom"
+import {useEffect, useState} from "react"
+import axios from "axios"
 
 export default function SinglePost() {
+  const location = useLocation()
+  const path = location.pathname.split("/")[2]
+  const [post, setPost] = useState({})
+
+  useEffect(() => {
+      const getPost = async () => {
+          const res = await axios.get("/posts/" + path)
+          setPost(res.data)
+      }
+      getPost()
+  }, [path])
+  
   return (
   <div className="singlePost">
       <div className="singlePostWrapper">
-          <img 
-            src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" 
-            alt=""
-            className="singlePostImg"
-        />
+          {post.photo && (
+               <img 
+               src={post.photo} 
+               alt=""
+               className="singlePostImg"
+           />
+          )}
         <h1 className="singlePostTitle">
-            Lorem ipsum dolor sit amet.
+            {post.title}
             <div className="singlePostEdit">
                 <i className="singlePostIcon far fa-edit"></i>
                 <i className="singlePostIcon  far fa-trash-alt"></i>
             </div>
         </h1>
         <div className="singlePostInfo">
-            <span className="singlePostAuthor">Author: <b>Dimon</b></span>
-            <span className="singlePostDate">1 hour ago</span>
+            <span className="singlePostAuthor">Author: <b>{post.username}</b></span>
+            <span className="singlePostDate">{new Date(post.createdAt).toDateString()}</span>
         </div>
         <p className="singlePostDesc">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque magna ipsum, finibus quis arcu non, varius porttitor augue. Quisque fermentum, dolor ac imperdiet viverra, lorem eros tempus ligula, sed eleifend dui turpis ac odio. Morbi dignissim gravida tempor. Sed lacinia justo libero, ut posuere dolor dictum et. Donec ut nisi ornare, rutrum lectus ac, gravida purus. Sed diam massa, venenatis vitae laoreet quis, bibendum auctor enim. Pellentesque sollicitudin ipsum laoreet sapien tempus consectetur. Mauris malesuada sed mauris ac finibus. Donec vitae lacus ultrices, varius tellus vitae, rutrum lorem. Phasellus posuere, lectus a condimentum euismod, lectus purus suscipit arcu, vel pharetra turpis lectus sit amet ligula. Morbi quis auctor lectus. Sed ut congue metus. Donec vel magna vitae libero efficitur rhoncus. Donec scelerisque scelerisque dolor. Phasellus semper enim nulla, non malesuada quam placerat quis. Morbi non ex id elit convallis sollicitudin nec at nisl.
-        Phasellus posuere, lectus a condimentum euismod, lectus purus suscipit arcu, vel pharetra turpis lectus sit amet ligula. Morbi quis auctor lectus. Sed ut congue metus. Donec vel magna vitae libero efficitur rhoncus. Donec scelerisque scelerisque dolor. Phasellus semper enim nulla, non malesuada quam placerat quis. Morbi non ex id elit convallis sollicitudin nec at nisl.
+           {post.desc}
         </p>
       </div>
   </div>
