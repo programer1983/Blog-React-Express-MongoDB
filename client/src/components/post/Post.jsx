174d382f97888c46bb6ac0f7e033b112cdf20194
programer1983 <programer972@gmail.com>
2022-02-02 10:57:02 +0200

    installation of axios, request to the server, forwarding data to posts and output visually;

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,28 +1,30 @@
 import "./post.css";
+import {Link} from "react-router-dom"
 
-export default function Post() {
+export default function Post({post}) {
   return (
     <div className="post">
+      {post.photo && (
         <img
-           className="postImage"
-           src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" 
-           alt="" 
-        />
+        className="postImage"
+        src={post.photo} 
+        alt="" 
+     /> 
+      )}
         <div className="postInfo">
             <div className="postCats">
-                <span className="postCat">Music</span>
-                <span className="postCat">Life</span>
+              {post.categories.map((c) => (
+                <span className="postCat">{c.name}</span>
+              ))}
             </div>
-            <span className="postTitle">
-                Lorem ipsum dolor sit amet. 
-            </span>
+            <Link to={`/post/${post._id}`} className="link">
+                <span className="postTitle">{post.title} </span>
+            </Link>
             <hr />
-            <span className="postDate">1 hour ago </span>
+            <span className="postDate">{new Date(post.createdAt).toDateString()} </span>
         </div>
         <p className="postDesc">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum lobortis odio quis hendrerit. Cras sagittis magna velit, a bibendum augue tincidunt vel. In eu ullamcorper nulla.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum lobortis odio quis hendrerit. Cras sagittis magna velit, a bibendum augue tincidunt vel. In eu ullamcorper nulla.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum lobortis odio quis hendrerit. Cras sagittis magna velit, a bibendum augue tincidunt vel. In eu ullamcorper nulla.
+          {post.desc}
         </p>
     </div>
   ) 
