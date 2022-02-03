84d053cbc161a372378d2dcb0f7896662c091f1e
programer1983 <programer972@gmail.com>
2022-02-03 13:51:09 +0200

    Link  to posts by category;

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,7 @@
 import "./sidebar.css"
 import {useEffect, useState} from "react"
 import axios from "axios"
+import {Link} from "react-router-dom"
 
 export default function Sidebar() {
     const [cats, setCats] = useState([])
@@ -28,7 +29,9 @@ export default function Sidebar() {
                 <span className="sidebarTitle">CATEGORIES</span>
                 <ul className="sidebarList">
                     {cats.map((c) => (
-                       <li className="sidebarListItem">{c.name}</li>
+                        <Link to={`/?cat=${c.name}`} className="link">
+                           <li className="sidebarListItem">{c.name}</li>
+                        </Link>
                     ))}
                 </ul>
             </div>
